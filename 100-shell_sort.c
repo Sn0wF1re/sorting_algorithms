16779e3ce528b873dd67a7a357ec00b81fce7b51
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-11-08 01:58:11 -0800

    Fix code to sort array using shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,7 +11,10 @@ void shell_sort(int *array, size_t size)
 	int temp;
 	size_t interval = 1, i, j;
 
-	for (interval = interval * 3 + 1; interval > 0; interval = (interval - 1) / 3)
+	while (interval <= size / 3)
+		interval = interval * 3 + 1;
+
+	while (interval > 0)
 	{
 		for (i = interval; i < size; i += 1)
 		{
@@ -23,6 +26,7 @@ void shell_sort(int *array, size_t size)
 
 			array[j] = temp;
 		}
+		interval = (interval - 1) / 3;
 		print_array(array, size);
 	}
 }

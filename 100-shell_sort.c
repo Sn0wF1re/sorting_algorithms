ee7d1eb868187e9ce191ccaa654f983dae6cb076
Clintonnyakundi <mokayaclinton34@gmail.com>
2022-11-08 01:43:27 -0800

    Fix shell_sort function

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,24 +11,18 @@ void shell_sort(int *array, size_t size)
 	int temp;
 	size_t interval = 1, i, j;
 
-	while (interval <= size / 3)
-		interval = interval * 3 + 1;
-
-	while (interval > 0)
+	for (interval = interval * 3 + 1; interval > 0; interval = (interval - 1) / 3)
 	{
 		for (i = interval; i < size; i++)
 		{
 			temp = array[i];
-			j = i;
-			while (j >= interval && array[j - interval] >= temp)
-			{
+
+			for (j = i; j >= interval && array[j - interval] >= temp;
+				j -= interval)
 				array[j] = array[j - interval];
-				j -= interval;
-			}
+
 			array[j] = temp;
 		}
-
-		interval = (interval - 1) / 3;
 		print_array(array, size);
 	}
 }
